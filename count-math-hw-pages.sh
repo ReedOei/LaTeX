44e084c34773799f1bf80a2cc8e92befa39e2c5e
Reed Oei <oei.reed@gmail.com>
2021-03-18 14:11:48 -0500

    Add "inline" to enumitem import

--- count-math-hw-pages.sh
+++ count-math-hw-pages.sh
@@ -8,6 +8,7 @@ fi
 search_path="$1"
 search_path_filter=".*"
 output_file="out.pdf"
+search_files_filter="main.pdf"
 
 if [[ -n "$2" ]]; then
     search_path_filter="$2"
@@ -17,8 +18,12 @@ if [[ -n "$3" ]]; then
     output_file="$3"
 fi
 
+if [[ -n "$4" ]]; then
+    search_files_filter="$4"
+fi
+
 # TODO: Should search for names other than main.pdf
-files="$(find "$search_path" -name "main.pdf" | grep -E "$search_path_filter" | tr "\n" " ")"
+files="$(find "$search_path" -name "$search_files_filter" | grep -E "$search_path_filter" | tr "\n" " ")"
 echo "$files"
 pdfunite $files "$output_file"
 
